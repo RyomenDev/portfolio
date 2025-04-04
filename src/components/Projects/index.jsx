193dff599cca1654f867098af327432877c66c23
RyomenDev <aakashvatsh007@gmail.com>
2025-04-04 06:56:11 +0530

    fix:deployment1

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -1,20 +1,13 @@
 import ExperienceAccord from "./projectsAccord/Projects.jsx";
-import ProjectDetails from "./projects/projects.jsx";
-// import ProjectDetails from "./projects/Project.jsx";
-// import { repos } from "../../editable-stuff/config";
+// import ProjectDetails from "./projects/projects.jsx";
+import ProjectDetails from "./projectDetails/projects.jsx";
+
 
 const Project = ({ theme }) => {
   return (
     <>
       <ExperienceAccord theme={theme} />
       <ProjectDetails />
-
-      {/* <ProjectDetails
-        heading={repos.heading}
-        username={repos.gitHubUsername}
-        length={repos.reposLength}
-        specific={repos.specificRepos}
-      /> */}
     </>
   );
 };
